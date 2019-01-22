6ca43ec3a6952a556793210c905e08853238a6dd
Steven Shaw <steven@steshaw.org>
2019-01-22 18:48:40 +1000

    Bump pinned nixpkgs version
    Tracking https://nixos.org/channels/nixpkgs-unstable

--- hello/.config/nix/default.nix
+++ hello/.config/nix/default.nix
@@ -1,11 +1,10 @@
 with builtins;
 let
-  importJSON = path: fromJSON (readFile path);
-
-  inherit ( importJSON ./nixpkgs.json ) rev sha256;
-
   # From `nix eval nixpkgs.lib.version`.
-  nixPkgsVersion = "19.03pre166683.088cb13aee0";
+  nixPkgsVersion = "19.03pre167171.5476f592c56";
+
+  rev = "5476f592c56";
+  sha256 = "sha256:0gx5gw82f9j20q8y3wq1bgfdl56859zhmw0zl2cc361l6yyyz0jp";
 
   nixpkgs = fetchTarball {
     name = "my-nixpkgs-" + nixPkgsVersion;

d0bc1e2138cc328d15f22f23374ccb65fdafd4ad
Steven Shaw <steven@steshaw.org>
2019-01-30 11:31:54 +1000

    Use Bash 5.0

--- pills/07-working-derivation/builder.bash
+++ pills/07-working-derivation/builder.bash
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 echo Arguments: "$@"
 
 echo
--- pills/07-working-derivation/default.nix
+++ pills/07-working-derivation/default.nix
@@ -4,10 +4,10 @@ stdenv.mkDerivation {
 */
 derivation {
   name = "foo-bar";
-  builder = "${bash}/bin/bash";
-  system = builtins.currentSystem;
-  args = [ ./builder.sh ];
+  builder = "${bash_5}/bin/bash";
+  args = [ ./builder.bash ];
   bust = 5;
+  system = builtins.currentSystem;
 #  ps = "${coreutils}/bin/ps";
 #  buildInputs = [ coreutils ];
 }

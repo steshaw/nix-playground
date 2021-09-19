57e6202b6449af628a35a1fe1adc1201bd9160b5
Steven Shaw <steven@steshaw.org>
2021-09-19 16:36:11 +1000

    Shellmet

--- practical-nix-flakes/hello.hs/app/Main.hs
+++ practical-nix-flakes/hello.hs/app/Main.hs
@@ -1,4 +1,27 @@
+{-# language OverloadedStrings #-}
+
 module Main where
 
+import Shellmet
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+
+badCommand :: IO ()
+badCommand = "ls" ["--XXX"]
+
+tshow :: (Show a) => a -> T.Text
+tshow = T.pack . show
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  putStrLn "Hello, Haskell!"
+  "echo" ["one", "two", "three"]
+  "ls" ["-l"]
+  badCommand $? T.putStrLn "Illegal command!"
+  ok <- isSuccess badCommand
+  T.putStrLn $ "isSuccess = " <> tshow ok
+  let file = "LICENCE"
+  text <- "cat" $| [file]
+  let count = T.pack $ show $ length $ T.lines text
+  "echo" ["Number of lines in " <> file <> ": " <> count]

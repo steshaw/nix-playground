b0050199d2ab3c95b175b4a2d74aad293d8fc101
Steven Shaw <steven@steshaw.org>
2021-09-19 15:03:57 +1000

    Using flake-utils

--- practical-nix-flakes/little/flake.nix
+++ practical-nix-flakes/little/flake.nix
@@ -1,17 +1,20 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/release-21.05";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in {
-      packages.x86_64-linux.hello = pkgs.hello;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.hello = pkgs.hello;
 
-      devShell.x86_64-linux = pkgs.mkShell {
-        buildInputs = [
-          self.packages.x86_64-linux.hello
-          pkgs.cowsay
-        ];
-      };
-    };
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            self.packages.${system}.hello
+            pkgs.cowsay
+          ];
+        };
+      }
+    );
 }

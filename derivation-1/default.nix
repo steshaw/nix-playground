eccee2bf36dcb4e38c21288b3d8ec70ec2fc905b
Steven Shaw <steven@steshaw.org>
2022-09-07 12:23:18 +1000

    Hello pkg

--- derivation-1/default.nix
+++ derivation-1/default.nix
@@ -3,7 +3,11 @@ let
     "https://github.com/NixOS/nixpkgs/archive/release-21.05.tar.gz";
   pkgs = import nixpkgs {
     overlays = [ ];
-    config = {};
+    config = { };
   };
 in
-builtins.length (builtins.attrNames pkgs)
+builtins.trace
+  ("pkgs.length = " +
+  toString (builtins.length (builtins.attrNames pkgs))
+  )
+  pkgs.hello

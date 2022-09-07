1e74eabaa233b54cc33c4c3f1ca00e733e8781b6
Steven Shaw <steven@steshaw.org>
2022-09-07 12:39:25 +1000

    Pin

--- derivation-1/default.nix
+++ derivation-1/default.nix
@@ -1,6 +1,7 @@
 let
+  # Pin to commit of release-22.05.
   nixpkgs = builtins.fetchTarball
-    "https://github.com/NixOS/nixpkgs/archive/release-21.05.tar.gz";
+    "https://github.com/NixOS/nixpkgs/archive/5aeb3fb419b07a4ed5a55e4a500b8a0bb6d12c75.tar.gz";
   pkgs = import nixpkgs {
     overlays = [ ];
     config = { };

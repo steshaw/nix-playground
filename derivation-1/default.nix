b49d811ead6749fce1350fe51a46d966e6aa5792
Steven Shaw <steven@steshaw.org>
2022-09-07 12:17:16 +1000

    Purify

--- derivation-1/default.nix
+++ derivation-1/default.nix
@@ -1,6 +1,9 @@
 let
   nixpkgs = builtins.fetchTarball
-    https://github.com/NixOS/nixpkgs/archive/release-21.05.tar.gz;
-  pkgs = import nixpkgs {};
+    "https://github.com/NixOS/nixpkgs/archive/release-21.05.tar.gz";
+  pkgs = import nixpkgs {
+    overlays = [ ];
+    config = {};
+  };
 in
 builtins.length (builtins.attrNames pkgs)

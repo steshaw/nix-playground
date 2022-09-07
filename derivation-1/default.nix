eb8b97a27725103ad787f8a73b016c64c02a694f
Steven Shaw <steven@steshaw.org>
2022-09-07 12:12:37 +1000

    import

--- derivation-1/default.nix
+++ derivation-1/default.nix
@@ -1,5 +1,6 @@
 let
   nixpkgs = builtins.fetchTarball
     https://github.com/NixOS/nixpkgs/archive/release-21.05.tar.gz;
+  pkgs = import nixpkgs {};
 in
-nixpkgs
+builtins.length (builtins.attrNames pkgs)

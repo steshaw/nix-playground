cd2b106dfdd049dd2a21083be88435a8306cffa7
Steven Shaw <steven.shaw@tweag.io>
2023-01-01 18:22:46 +1000

    Output vars

--- pills/06-our-first-derivation/hi/default.nix
+++ pills/06-our-first-derivation/hi/default.nix
@@ -2,4 +2,19 @@ derivation {
   name = "foo";
   builder = ./build;
   system = "x86_64-linux";
+
+  # Strings
+  v1 = "this is a string";
+  # Numbers
+  v2 = 42;
+  # Path
+  v3 = ./default.nix;
+  # Derivation
+  v4 = "TODO";
+  # Lists
+  v5 = [ "a-string" 1 2 3 "hi" ./default.nix ./build ];
+  # True, false, null
+  v6 = true;
+  v7 = false;
+  v8 = null;
 }

cfcba9af8a9337362a96fd86fca5a32928817769
Steven Shaw <steven.shaw@tweag.io>
2023-01-03 12:29:26 +1000

    args

--- pills/06-our-first-derivation/hi/default.nix
+++ pills/06-our-first-derivation/hi/default.nix
@@ -1,20 +1,22 @@
-derivation {
+derivation rec {
   name = "foo";
   builder = ./build;
   system = "x86_64-linux";
 
   # Strings
-  v1 = "this is a string";
+  V1 = "this is a string";
   # Numbers
-  v2 = 42;
+  V2 = 42;
   # Path
-  v3 = ./default.nix;
+  V3 = ./default.nix;
   # Derivation
-  v4 = "TODO";
+  V4 = "TODO";
   # Lists
-  v5 = [ "a-string" 1 2 3 "hi" ./default.nix ./build ];
+  V5 = [ "a-string" 1 2 3 "hi" ./default.nix ./build ];
   # True, false, null
-  v6 = true;
-  v7 = false;
-  v8 = null;
+  V6 = true;
+  V7 = false;
+  V8 = null;
+
+  args = [ V1 V2 V3 V4 V5 V6 V7 V8 ];
 }

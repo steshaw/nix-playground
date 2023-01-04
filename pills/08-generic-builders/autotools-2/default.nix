3de9b4ab71f89762bb461e49cdd046273553aa79
Steven Shaw <steven.shaw@tweag.io>
2023-01-04 19:43:00 +1000

    Include sha256

--- pills/08-generic-builders/autotools-2/default.nix
+++ pills/08-generic-builders/autotools-2/default.nix
@@ -4,5 +4,8 @@ let
 in
 autotoolsDerivation {
   name = "hello";
-  src = builtins.fetchurl http://ftp.gnu.org/gnu/hello/hello-2.12.tar.gz;
+  src = builtins.fetchurl {
+    url = "http://ftp.gnu.org/gnu/hello/hello-2.12.tar.gz";
+    sha256 = "1ayhp9v4m4rdhjmnl2bq3cibrbqqkgjbl3s7yk2nhlh8vj3ay16g";
+  };
 }

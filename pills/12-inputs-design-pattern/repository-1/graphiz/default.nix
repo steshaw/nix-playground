f9c0338bbf54a7220e4e230588efccfc0e9f448f
Steven Shaw <steven.shaw@tweag.io>
2023-01-06 17:33:48 +1000

    Move to default.nix

--- pills/12-inputs-design-pattern/repository-1/graphiz/default.nix
+++ pills/12-inputs-design-pattern/repository-1/graphiz/default.nix
@@ -1,1 +1,17 @@
-import ./graphiz.nix
+{pkgs ? import <nixpkgs> {} }:
+let
+  autotoolsDerivation = import ./autotools.nix pkgs;
+in
+with pkgs; autotoolsDerivation {
+  name = "graphviz";
+  src = builtins.fetchurl {
+    url =
+      "https://gitlab.com/api/v4/projects/4207231/packages/generic/graphviz-releases/2.49.3/graphviz-2.49.3.tar.gz";
+    sha256 = "1gycwaq6lnz03pizfibipq1j15hb975xdb0qsaagkq4qvhyj16zp";
+  };
+  buildInputs = [
+    pkg-config
+    gd
+    gd.dev
+  ];
+}

edcf1e76869c20efe84deb903aa227f45b1252d0
Steven Shaw <steven.shaw@tweag.io>
2023-01-08 11:34:19 +1000

    Split out callPackage testing
    Otherwise building with flakes fails because of linkFarm.

--- pills/13-call-package-design-pattern/repository/default.nix
+++ pills/13-call-package-design-pattern/repository/default.nix
@@ -1,10 +1,5 @@
 { pkgs ? import <nixpkgs> { } }:
 let
-  callPackage = set: f: overrides: f (
-    builtins.intersectAttrs
-      (builtins.functionArgs f)
-      set // overrides
-  );
   autotoolsDerivation = import ./autotools pkgs;
 in
 with pkgs;
@@ -15,19 +10,8 @@ let
     inherit gd;
     inherit pkg-config;
   };
-  add = {a ? 3, b}: a + b;
-  values = {
-    a = 10;
-    b = 32;
-    c = 42;
-    d = 99;
-  };
 in
 {
-  inherit callPackage;
-  test1 = callPackage values add {};
-  test2 = callPackage values add { a = 0; };
-
   hello = import ./hello {
     inherit autotoolsDerivation;
   };

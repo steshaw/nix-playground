e4ce7ffc5e14503828803caeadec27050c73e288
Steven Shaw <steven.shaw@tweag.io>
2023-01-08 11:12:04 +1000

    Add callPackage

--- pills/13-call-package-design-pattern/repository/default.nix
+++ pills/13-call-package-design-pattern/repository/default.nix
@@ -1,5 +1,10 @@
 { pkgs ? import <nixpkgs> { } }:
 let
+  callPackage = set: f: f (
+    builtins.intersectAttrs
+      (builtins.functionArgs f)
+      set
+  );
   autotoolsDerivation = import ./autotools pkgs;
 in
 with pkgs;
@@ -12,6 +17,14 @@ let
   };
 in
 {
+  add = {a ? 3, b}: a + b;
+  values = {
+    a = 10;
+    b = 32;
+    c = 42;
+    d = 99;
+  };
+  inherit callPackage;
   hello = import ./hello {
     inherit autotoolsDerivation;
   };

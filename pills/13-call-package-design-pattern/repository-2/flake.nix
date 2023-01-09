5d57b8842ff01b748acf932aabfea54014be5a0b
Steven Shaw <steven.shaw@tweag.io>
2023-01-09 15:36:23 +1000

    Initialise repository-2

---
+++ pills/13-call-package-design-pattern/repository-2/flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-22.11;
+  inputs.flake-utils.url = github:numtide/flake-utils;
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        system-nixpkgs = nixpkgs.legacyPackages.${system};
+        our-pkgs = (import ./default.nix) {
+          nixpkgs = system-nixpkgs;
+        };
+        all = system-nixpkgs.linkFarm "all" our-pkgs;
+      in
+      {
+        packages = our-pkgs // {
+          inherit all;
+          default = all;
+        };
+      }
+    );
+}

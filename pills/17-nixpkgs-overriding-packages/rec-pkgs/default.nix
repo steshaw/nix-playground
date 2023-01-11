bc670b883e7a7a094e2693260e5dfa93de5f0720
Steven Shaw <steven.shaw@tweag.io>
2023-01-11 14:49:40 +1000

    Overriding newpkgs examples

--- pills/17-nixpkgs-overriding-packages/rec-pkgs/default.nix
+++ pills/17-nixpkgs-overriding-packages/rec-pkgs/default.nix
@@ -1,10 +1,12 @@
-{ useFix ? false }:
 let
   pkgs = self:
     builtins.trace "pkgs" { a = 3; b = 4; c = self.a + self.b; };
   fix = f: let r = builtins.trace "fix.f" (f r); in r;
+  overrides = { a = 1; b = 2; };
 in
 {
   pkgs = pkgs (pkgs pkgs);
   pkgs-fix = fix pkgs;
+  newpkgs = let newpkgs = pkgs (newpkgs // overrides); in newpkgs;
+  newpkgs2 = let newpkgs = pkgs (newpkgs // overrides); in newpkgs // overrides;
 }

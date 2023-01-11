dc387ba9fde12b361d8d25a62bc4727aee2a5df1
Steven Shaw <steven.shaw@tweag.io>
2023-01-11 14:44:14 +1000

    Use --attr

--- pills/17-nixpkgs-overriding-packages/rec-pkgs/default.nix
+++ pills/17-nixpkgs-overriding-packages/rec-pkgs/default.nix
@@ -4,4 +4,7 @@ let
     builtins.trace "pkgs" { a = 3; b = 4; c = self.a + self.b; };
   fix = f: let r = builtins.trace "fix.f" (f r); in r;
 in
-  if useFix then fix pkgs else pkgs (pkgs pkgs)
+{
+  pkgs = pkgs (pkgs pkgs);
+  pkgs-fix = fix pkgs;
+}

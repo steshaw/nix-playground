11f98fe07ac84f5bfe9f490391e68e151ef92c59
Steven Shaw <steven.shaw@tweag.io>
2023-01-11 15:09:00 +1000

    Resolve in specific order

--- pills/17-nixpkgs-overriding-packages/rec-pkgs/default.nix
+++ pills/17-nixpkgs-overriding-packages/rec-pkgs/default.nix
@@ -3,10 +3,15 @@ let
     builtins.trace "pkgs" { a = 3; b = 4; c = self.a + self.b; };
   fix = f: let r = builtins.trace "fix.f" (f r); in r;
   overrides = { a = 1; b = 2; };
-in
-{
-  pkgs = pkgs (pkgs pkgs);
+in let
+  pkgs-app = pkgs (pkgs pkgs);
   pkgs-fix = fix pkgs;
   newpkgs = let newpkgs = pkgs (newpkgs // overrides); in newpkgs;
   newpkgs2 = let newpkgs = pkgs (newpkgs // overrides); in newpkgs // overrides;
+  all = {
+    inherit pkgs-app pkgs-fix newpkgs newpkgs2;
+  };
+in
+all // {
+  list = ["pkgs-app" "pkgs-fix" "newpkgs" "newpkgs2"];
 }

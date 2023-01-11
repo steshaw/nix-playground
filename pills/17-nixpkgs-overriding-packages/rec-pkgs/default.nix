058a2e236d3f958d47d9c1f385467e34a584d9ab
Steven Shaw <steven.shaw@tweag.io>
2023-01-11 15:18:49 +1000

    Add another dependent variable

--- pills/17-nixpkgs-overriding-packages/rec-pkgs/default.nix
+++ pills/17-nixpkgs-overriding-packages/rec-pkgs/default.nix
@@ -1,10 +1,15 @@
 let
   pkgs = self:
-    builtins.trace "pkgs" { a = 3; b = 4; c = self.a + self.b; };
+    builtins.trace "pkgs" {
+      a = 3;
+      b = 4;
+      c = self.a + self.b;
+      d = self.c * self.c;
+    };
   fix = f: let r = builtins.trace "fix.f" (f r); in r;
   overrides = { a = 1; b = 2; };
 in let
-  pkgs-app = pkgs (pkgs pkgs);
+  pkgs-app = pkgs (pkgs (pkgs pkgs));
   pkgs-fix = fix pkgs;
   newpkgs = let newpkgs = pkgs (newpkgs // overrides); in newpkgs;
   newpkgs2 = let newpkgs = pkgs (newpkgs // overrides); in newpkgs // overrides;

6a237f52745a2418f346254f7e5bd4f0e69a331f
Steven Shaw <steven.shaw@tweag.io>
2023-01-11 17:30:28 +1000

    rec-pkgs-3

--- pills/17-nixpkgs-overriding-packages/rec-pkgs-3/default.nix
+++ pills/17-nixpkgs-overriding-packages/rec-pkgs-3/default.nix
@@ -1,32 +1,30 @@
 let
   fix = f: let r = builtins.trace "fix.f" (f r); in r;
 
-  pkgsO = overrides: self:
+  pkgs = self:
     builtins.trace "pkgs" {
       a = 3;
       b = 4;
       c = self.a + self.b;
       d = self.c * self.c;
-    } // overrides;
+      f = 9;
+    };
 
-  overrides = {
+  overrides-mixin = self: {
     a = 1;
-    b = 2;
+    b = self.f + 2;
   };
 
 in let
-  pkgs = pkgsO {};
   pkgs-app = pkgs (pkgs (pkgs pkgs));
   pkgs-fix = fix pkgs;
-  newpkgs = let newpkgs = pkgs (newpkgs // overrides); in newpkgs;
-  newpkgs2 = let newpkgs = pkgs (newpkgs // overrides); in newpkgs // overrides;
-  #newpkgs3 = ov: let newpkgs = pkgs (newpkgs // ov); in newpkgs // ov;
+#  newpkgs = let newpkgs = pkgs (newpkgs // overrides); in newpkgs;
+#  newpkgs2 = let newpkgs = pkgs (newpkgs // overrides); in newpkgs // overrides;
 
-  opkgs = fix (pkgsO overrides);
   all = {
-    inherit pkgs-app pkgs-fix newpkgs newpkgs2 opkgs;
+    inherit pkgs-app pkgs-fix;
   };
 in
 all // {
-  list = ["pkgs-app" "pkgs-fix" "newpkgs" "newpkgs2" "opkgs"];
+  list = ["pkgs-app" "pkgs-fix"];
 }

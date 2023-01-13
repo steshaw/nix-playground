1bf43853ce05f33375edaf8ace54145af352d2b3
Steven Shaw <steven.shaw@tweag.io>
2023-01-13 13:26:21 +1000

    Fixed output

---
+++ pills/18-nix-store-paths/fixed.nix
@@ -0,0 +1,8 @@
+derivation {
+  name = "bar";
+  system = "x86_64-linux";
+  builder = "none";
+  outputHashMode = "flat";
+  outputHashAlgo = "sha256";
+  outputHash = "f3f3c4763037e059b4d834eaf68595bbc02ba19f6d2a500dce06d124e2cd99bb";
+}

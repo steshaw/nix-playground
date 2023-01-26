b3a331536ba0f73b5ba0c17e1088e57a37343403
Steven Shaw <steven.shaw@tweag.io>
2023-01-26 12:49:38 +1000

    Use flake-utils

--- direnv-flakes/flake.nix
+++ direnv-flakes/flake.nix
@@ -1,13 +1,15 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = { self, nixpkgs }:
-    let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-    in
-    {
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        buildInputs = with pkgs; [ go_1_19 terraform ];
-      };
-    };
+  outputs = { self, nixpkgs, flake-utils }:
+    let inherit (flake-utils.lib) eachSystem allSystems; in
+    eachSystem allSystems (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ go_1_19 terraform ];
+        };
+      }
+    );
 }

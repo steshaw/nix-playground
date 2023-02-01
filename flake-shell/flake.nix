b242e4e2b089e5b370ba1767a2f8741235a40229
Steven Shaw <steven.shaw@tweag.io>
2023-02-01 18:36:45 +1000

    Use flake-utils eachDefaultSystem

--- flake-shell/flake.nix
+++ flake-shell/flake.nix
@@ -1,16 +1,20 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
-  outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default =
-      let pkgs = nixpkgs.legacyPackages."x86_64-linux";
-      in pkgs.mkShell {
-        buildInputs = with pkgs; [
-          go_1_18
-          nodePackages.prettier
-          cargo
-          python38
-          openssl
-        ];
-      };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: {
+      devShells.default =
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go_1_18
+            nodePackages.prettier
+            cargo
+            python38
+            openssl
+          ];
+        };
+    });
 }

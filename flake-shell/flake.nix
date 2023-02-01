be4dbb6df48c3e74339a1df4c6021608ce82c53c
Steven Shaw <steven.shaw@tweag.io>
2023-02-01 17:49:17 +1000

    Minimal changes for "working" shell

--- flake-shell/flake.nix
+++ flake-shell/flake.nix
@@ -1,11 +1,15 @@
 {
-  devShells.default = pkgs.mkShell {
-    buildInputs = with pkgs; [
-      go_1_18
-      nodePackages.prettier
-      cargo
-      python38
-      openssl
-    ];
+  outputs = { self, nixpkgs }: {
+    devShells.x86_64-linux.default =
+      let pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      in pkgs.mkShell {
+        buildInputs = with pkgs; [
+          go_1_18
+          nodePackages.prettier
+          cargo
+          python38
+          openssl
+        ];
+      };
   };
 }

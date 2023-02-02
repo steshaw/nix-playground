3dabcf982d590a6a999695a01990baa7ba21c947
Steven Shaw <steven.shaw@tweag.io>
2023-02-02 17:54:18 +1000

    Build docker image of flask-example

---
+++ python-flask-example/docker.nix
@@ -0,0 +1,23 @@
+let
+  overlay = self: super: {
+    flask-example = self.poetry2nix.mkPoetryApplication {
+      projectDir = ./.;
+      python = self.python3; # XXX: Why?
+    };
+  };
+  hostPkgs = import <nixpkgs> {
+    overlays = [overlay];
+  };
+  linuxPkgs = import <nixpkgs> {
+    overlays = [overlay];
+    system = "x86_64-linux";
+  };
+in {
+  inherit (hostPkgs) flask-example;
+
+  docker = hostPkgs.dockerTools.streamLayeredImage {
+    name = "flask-example";
+    contents = [ linuxPkgs.flask-example ];
+    config.Cmd = ["flask-example"];
+  };
+}

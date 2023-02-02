88da3406efd10a198ea37b4d6460aa664e20c4a1
Steven Shaw <steven.shaw@tweag.io>
2023-02-02 16:55:04 +1000

    Add ability to run via poetry

--- python-flask-example/app.py
+++ python-flask-example/app.py
@@ -7,5 +7,8 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
-if __name__ == '__main__':
+def main():
     app.run(host="0.0.0.0", port=8080)
+
+if __name__ == '__main__':
+    main()
